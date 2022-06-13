3f2d1476618b98ffbe2afafcabcdf4fdcc298f07
Maratib Ali Khan <maratib@gmail.com>
2022-06-13 10:34:22 -0700

    initial commit

--- src/test/java/com/example/test1/SingleFileTest.java
+++ src/test/java/com/example/test1/SingleFileTest.java
@@ -2,12 +2,8 @@ package com.example.test1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-// @SpringBootApplication
-// @ContextConfiguration(locations = { "/application.properties" })
 @SpringBootTest
 public class SingleFileTest {
 
